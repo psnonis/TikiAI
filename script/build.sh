44d1b8440b6444fc1d8e92f9dc0c6665bd3abcec
Pri <priyankara.nonis@gmail.com>
2019-07-06 14:40:39 -0700

    Adding Shebangs

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 image=${1-pythia}
 echo Building ${image}
 docker build -t w251-final/${image} ${image} | tee ${image}/build.log | ack --passthru '^Step.*'
--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 image=${1-pythia}
 echo Running ${image}
 docker run --rm -v ${1}:/final/wip -it w251-final/${image} bash
