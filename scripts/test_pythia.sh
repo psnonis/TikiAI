d9ce2f33aebd85f8e76656d6fe80ef084de63a94
root <root@final.w251.cloud>
2019-07-13 18:35:43 +0000

    Pythia Test Script Update

--- scripts/test_pythia.sh
+++ scripts/test_pythia.sh
@@ -2,11 +2,10 @@
 
       root=$(readlink -f ${0} | xargs dirname | xargs dirname)
 containers=$(realpath --relative-to=${PWD} ${root}/containers)
-   context=${1-pythia}
-    expose=${2-5000}
+      host=${1-localhost}
 repository=psnonis/w251-final
 
 curl \
   -X POST \
-  -F "image=@${containers}/${context}/africa.jpg" \
-  localhost:5000/api/divine?question='where%20is%20this%20place%20%3F'
+  -F "image=@${containers}/pythia/app/africa.jpg" \
+  ${host}:5000/api/divine?question='where%20is%20this%20place%20%3F'
