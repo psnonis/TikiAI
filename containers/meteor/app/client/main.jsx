0c2f44a412654a4a0e82d75ac151accd20bd67ce
Pri <priyankara.nonis@gmail.com>
2019-07-22 22:19:19 -0700

    Work In Progress

--- containers/meteor/app/client/main.jsx
+++ containers/meteor/app/client/main.jsx
@@ -3,30 +3,9 @@ import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
 
 import React      from 'react'
-import App        from '/imports/ui/App'
+import Tiki       from '/imports/ui/Tiki'
 
 Meteor.startup(() =>
 {
-
-/*
-  const io   = require('socket.io-client')
-  let socket = io(`http://localhost:3003`)
-
-  socket.on('connect', () =>
-  {
-    console.log('Client Connected')
-  })
-  
-  socket.on('disconnect', () =>
-  {
-    console.log('Client Disconnected')
-  })
-
-  socket.on('counter', (value) =>
-  {
-    console.log(value)
-  })
-*/
-
-  render(<App />, document.getElementById('react-target'))
+  render(<Tiki />, document.getElementById('react-target'))
 })
--- containers/meteor/app/server/main.js
+++ containers/meteor/app/server/main.js
@@ -1,35 +1,14 @@
-import { Meteor } from 'meteor/meteor'
+import { Meteor        } from 'meteor/meteor'
+import { writeFileSync } from 'fs'
+import   superagent      from 'superagent'
+
+const PYTHIA = 'http://158.85.220.245:5000' // 'http://localhost:5000'
 
 console.log('server > main')
 
 Meteor.startup(() =>
 {
 })
-
-function dataURItoBlob(dataURI)
-{
-  var byteString;
-  if (dataURI.split(',')[0].indexOf('base64') >= 0)
-    byteString = atob(dataURI.split(',')[1])
-  else
-    byteString = unescape(dataURI.split(',')[1])
-
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-  var byteBuffer = new Uint8Array(byteString.length)
-  for (var i = 0; i < byteString.length; i++)
-  {
-    byteBuffer[i] = byteString.charCodeAt(i)
-  }
-
-  console.log(mimeString)
-
-  return new Blob([byteBuffer], {type: mimeString})
-}
-
-import superagent from 'superagent'
-import { writeFileSync } from 'fs'
-
-const PYTHIA = 'http://158.85.220.245:5000' // 'http://localhost:5000'
 
 Meteor.methods(
 {
@@ -38,7 +17,6 @@ Meteor.methods(
         console.log('server > main > apiDivine called')
 
         console.log(params)
-        console.log(params.snapshot.split(',')[1])
 
         var question = params.question
         var snapshot = Buffer.from(params.snapshot.split(',')[1], 'base64')
